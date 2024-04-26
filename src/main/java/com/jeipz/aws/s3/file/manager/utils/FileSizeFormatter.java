276a3a56c9d966e0db7b3b5c619c91a87a93c1c7
John Paul Rosario <rosario.jeipz@gmail.com>
2024-04-26 12:36:31 +0900

    Added file size formatter and format datetime

---
+++ src/main/java/com/jeipz/aws/s3/file/manager/utils/FileSizeFormatter.java
@@ -0,0 +1,13 @@
+package com.jeipz.aws.s3.file.manager.utils;
+
+import java.text.DecimalFormat;
+
+public class FileSizeFormatter {
+
+    public static String formatFileSize(long size) {
+        if (size <= 0) return "0 B";
+        final String[] units = {"B", "KB", "MB", "GB", "TB"};
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+    }
+}
